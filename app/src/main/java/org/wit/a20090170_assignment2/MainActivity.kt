cc8bb8aa10ed4d2a9fcf90e639d5292df1bbc97a
CaolanMaher <caolanmaher2@gmail.com>
2022-11-17 14:22:40 +0000

    Initial Commit

---
+++ app/src/main/java/org/wit/a20090170_assignment2/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.wit.a20090170_assignment2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
