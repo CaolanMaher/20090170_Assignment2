8b0b94d0c31d6e5ae3f9a0a5201de392133b0e6e
CaolanMaher <caolanmaher2@gmail.com>
2022-12-13 11:40:59 +0000

    Added Firebase Firestore Support for Adding, Updating, Deleting, Retrieving

---
+++ app/src/main/java/org/wit/a20090170_assignment2/models/RentalCarFireStore.kt
@@ -0,0 +1,100 @@
+package org.wit.a20090170_assignment2.models
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import org.wit.a20090170_assignment2.helpers.exists
+import timber.log.Timber.i
+import java.util.*
+
+class RentalCarFireStore : RentalCarStore{
+
+    private val db = FirebaseFirestore.getInstance()
+
+    private val rentalCar = RentalCarModel()
+
+    var rentalCars = mutableListOf<RentalCarModel>()
+
+    init {
+        db.collection("rentalCars")
+            .get()
+            .addOnCompleteListener {
+                //val rentalCar = RentalCarModel()
+                if(it.isSuccessful) {
+                    for(document in it.result!!) {
+                        rentalCar.brand = document.data.getValue("brand").toString()
+                        rentalCar.year = document.data.getValue("year").toString().toInt()
+                        rentalCars.add(rentalCar)
+                        i("CAR:" + rentalCar.brand + rentalCar.year)
+                    }
+                }
+            }
+    }
+
+    override fun findAll(): List<RentalCarModel> {
+        return rentalCars
+    }
+
+    override fun create(rentalCar: RentalCarModel) {
+        rentalCar.id = generateRandomId()
+
+        db.collection("rentalCars")
+            .add(rentalCar)
+            .addOnSuccessListener {
+                i("SUCCESSFULLY ADDED CAR")
+            }
+            .addOnFailureListener {
+                // to do
+            }
+        rentalCars.add(rentalCar)
+    }
+
+    override fun update(rentalCar: RentalCarModel) {
+        i("UPDATING")
+
+        var foundRentalCar: RentalCarModel? = rentalCars.find{ r -> r.id == rentalCar.id }
+        if(foundRentalCar != null) {
+
+            rentalCars.remove(rentalCar)
+
+            db.collection("rentalCars")
+                .get()
+                .addOnCompleteListener {
+                    if(it.isSuccessful) {
+                        for(document in it.result!!) {
+                            if(document.data.getValue("id").toString() == rentalCar.id.toString()) {
+                                // UPDATE
+                                document.reference.set(rentalCar)
+                            }
+                        }
+                    }
+                }
+        }
+    }
+
+    override fun delete(rentalCar: RentalCarModel) {
+        i("DELETING")
+
+        var foundRentalCar: RentalCarModel? = rentalCars.find{ r -> r.id == rentalCar.id }
+        if(foundRentalCar != null) {
+
+            rentalCars.remove(rentalCar)
+
+            db.collection("rentalCars")
+                .get()
+                .addOnCompleteListener {
+                    if(it.isSuccessful) {
+                        for(document in it.result!!) {
+                            if(document.data.getValue("id").toString() == rentalCar.id.toString()) {
+                                // DELETE
+                                document.reference.delete()
+                            }
+                        }
+                    }
+                }
+        }
+    }
+
+    private fun generateRandomId(): Long {
+        return Random().nextLong()
+    }
+}
